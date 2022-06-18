2403ac627ac4ec02c9c271cef2de7ea950a8c24c
GiggleLiu <cacate0129@gmail.com>
2022-06-18 00:02:43 -0400

    add shapes

---
+++ test/nodes.jl
@@ -0,0 +1,13 @@
+using Test
+using LuxorGraphPlot: angleof, closest_natural_point
+using LuxorGraphPlot
+using Luxor
+
+@testset "angleof and boundary" begin
+    @test angleof(Point(0.5*sqrt(3.0), 0.5)) ≈ π/6
+    n = node(box, O, 100, 100)
+    @test boundary(n, angleof(Point(0.5*sqrt(3.0), 0.5))) ≈ Point(50, 50/sqrt(3))
+    @test closest_natural_point(n, Point(100, 120)) ≈ Point(50, 50)
+    @test closest_natural_point(n, Point(130, -120)) ≈ Point(50, -50)
+    @test closest_natural_point(n, Point(130, -10)) ≈ Point(50, 0)
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,15 +1,10 @@
 using LuxorGraphPlot, Graphs
 using Test
 
-@testset "LuxorGraphPlot.jl" begin
-    locations = [(1.0, 2.0), (2.0, 3.0)]
-    @test show_graph(locations, [(1, 2)]) isa Any
-    @test show_graph([], []) isa Any
-    @test show_graph([], []; format=:pdf) isa Any
-    @test show_graph([], []; filename=tempname()*".svg") isa Any
-    graph = smallgraph(:petersen)
-    @test show_graph(graph) isa Any
-    # gallery
-    @test show_gallery([], [], (3, 3)) isa Any
-    @test show_gallery(graph, (2,4); vertex_configs=[rand(Bool, 15) for i=1:10], edge_configs=[rand(Bool, 15) for i=1:10]) isa Any
+@testset "nodes" begin
+    include("nodes.jl")
+end
+
+@testset "graphplot" begin
+    include("graphplot.jl")
 end
