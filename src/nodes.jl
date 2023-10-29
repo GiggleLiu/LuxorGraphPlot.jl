4294a7ee98c4d9b53e16159d9e03d9ce5902500e
GiggleLiu <cacate0129@gmail.com>
2023-10-29 17:57:10 +0800

    update

--- src/nodes.jl
+++ src/nodes.jl
@@ -1,5 +1,27 @@
 @enum Shape Circle Box Polygon Segment Dot
+const REQUIRED_PARAMS = Dict(
+    Circle => [:radius],
+    Box => [:width, :height],
+    Polygon => [:relpath],
+    Segment => [:relpath],
+    Dot => Symbol[]
+)
 
+const OPTIONAL_PARAMS = Dict(
+    Circle => Dict{Symbol, Any}(),
+    Box => Dict{Symbol, Any}(),
+    Polygon => Dict{Symbol, Any}(),
+    Segment => Dict{Symbol, Any}(:arrowstyle=>"-"),
+    Dot => Dict{Symbol, Any}()
+)
+
+"""
+### Required Keyword Arguments
+$REQUIRED_PARAMS
+
+### Optional Keyword Arguments
+$OPTIONAL_PARAMS
+"""
 struct Node
     shape::Shape
     loc::Point
@@ -7,37 +29,38 @@ struct Node
 end
 function Node(shape::Shape, loc; props...)
     d = Dict{Symbol, Any}(props)
-    check_props(shape, d)
+    check_props!(shape, d)
     return Node(shape, Point(loc), d)
 end
 ndot(x::Real, y::Real) = Node(Dot, (x, y))
 ndot(p::Point) = Node(shape, p)
 ncircle(loc, radius) = Node(Circle, loc; radius)
 nbox(loc, width, height) = Node(Box, loc; width, height)
-npolygon(loc, abspath) = Node(Polygon, loc; abspath)
+npolygon(loc, relpath) = Node(Polygon, loc; relpath)
 function nsegment(args...)
-    abspath = [Point(x) for x in args]
-    return Node(Segment, Point(0, 0); abspath)
+    relpath = [Point(x) for x in args]
+    return Node(Segment, Point(0, 0); relpath)
 end
 
-function check_props(shape, props)
-    @match shape begin
-        Circle => assert_has_props(shape, props, [:radius])
-        Box => assert_has_props(shape, props, [:width, :height])
-        Polygon => assert_has_props(shape, props, [:abspath])
-        Segment => assert_has_props(shape, props, [:abspath])
-        Dot => assert_has_props(shape, props, Symbol[])
-        _ => error("unknown shape: $shape")
-    end
+function check_props!(shape, props)
+    assert_has_props!(shape, props, REQUIRED_PARAMS[shape], OPTIONAL_PARAMS[shape])
 end
-function assert_has_props(shape, props, syms)
+function assert_has_props!(shape, props, syms, optional)
+    # required arguments
     for sym in syms
         if !haskey(props, sym)
             error("missing property (keyword argument) for shape $shape: $sym ")
         end
     end
+    # optional arguments
+    for (k, v) in optional
+        if !haskey(props, k)
+            props[k] = v
+        end
+    end
+    # not recognized arguments
     for (k, v) in props
-        if !(k ∈ syms)
+        if !(k ∈ syms || haskey(optional, k))
             @warn "property not recognized by shape $shape: $k"
         end
     end
@@ -55,8 +78,8 @@ function apply_action(n::Node, action)
     @match n.shape begin
         Circle => circle(n.loc, n.radius, action)
         Box => box(n.loc, n.width, n.height, action)
-        Polygon => poly(Ref(n.loc) .+ n.abspath, action; close=true)
-        Segment => line((Ref(n.loc) .+ n.abspath)..., action)
+        Polygon => poly(Ref(n.loc) .+ n.relpath, action; close=true)
+        Segment => line((Ref(n.loc) .+ n.relpath)..., action)
         Dot => circle(n.loc, 1, action)  # dot has unit radius
     end
 end
@@ -84,6 +107,8 @@ function boundary(n::Node, angle::Real)
         _ => error("can not get boundary point for shape: $(n.shape)")
     end
 end
+
+# get the path of a node
 function getpath(n::Node)
     @match n.shape begin
         Circle => error("getting path of a circle is not allowed!")
@@ -92,9 +117,9 @@ function getpath(n::Node)
             w, h = n.width, n.height
             [Point(x-w/2, y-h/2), Point(x-w/2, y+h/2), Point(x+w/2, y+h/2), Point(x+w/2, y-h/2)]
         end
-        Polygon => Ref(n.loc) .+ n.abspath
+        Polygon => Ref(n.loc) .+ n.relpath
         Dot => [n.loc]
-        Segment => Ref(n.loc) .+ n.abspath
+        Segment => Ref(n.loc) .+ n.relpath
     end
 end
 
@@ -112,27 +137,25 @@ function edge(::typeof(arrow), a::Point, b::Point, action=:path; kwargs...)
 end
 
 function get_connect_point(a::Node, b::Node; mode=:exact)
-    if a.shape == Circle
-        nints, ip1, ip2 =  intersectionlinecircle(a.loc, b.loc, a.loc, width(a)/2)
-        return ip1
-    else
-        if mode == :natural
-            return closest_natural_point(a, b.loc)
-        elseif mode == :exact
-            return boundary(a, angleof(b.loc-a.loc))
-        else
-            error("Connection point mode `:$(mode)` is not defined!")
+    @match a.shape begin
+        Circle => ndot(intersectionlinecircle(a.loc, b.loc, a.loc, a.radius)[2])
+        Dot => a
+        Segment => ndot(a.loc + a.relpath[end])  # the last node
+        Box || Polygon => @match mode begin
+            :natural => ndot(closest_natural_point(getpath(a), b.loc))
+            :exact => boundary(a, angleof(b.loc-a.loc))
+            _ => error("Connection point mode `:$(mode)` is not defined!")
         end
     end
 end
 
 angleof(p::Point) = atan(p.y, p.x)
 
-function closest_natural_point(n::Node, p::Point)
-    minval, idx = findmin(x->distance(p, x), n.path)
-    mid = i->midpoint(n.path[i], n.path[mod1(i+1, length(n.path))])
-    minval2, idx2 = findmin(i->distance(p, mid(i)), 1:length(n.path))
-    return minval > minval2 ? mid(idx2) : n.path[idx]
+function closest_natural_point(path, p::Point)
+    minval, idx = findmin(x->distance(p, x), path)
+    mid = i->midpoint(path[i], path[mod1(i+1, length(path))])
+    minval2, idx2 = findmin(i->distance(p, mid(i)), 1:length(path))
+    return minval > minval2 ? mid(idx2) : path[idx]
 end
 
 # labeled items
